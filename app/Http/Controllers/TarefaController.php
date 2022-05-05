1528a060d446236521c61b19e2e8915784e57be4
Vitor-Linaris <vitor.linaris@gmail.com>
2022-05-04 22:02:25 -0300

    Initial commit

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use App\Mail\NovaTarefaMail;
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\TarefasExport;
+
+class TarefaController extends Controller
+{
+    public function __construct() {
+        //$this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = auth()->user()->id;
+        $tarefas = Tarefa::where('user_id', $user_id)->paginate(10);
+        return view('tarefa.index', ['tarefas' => $tarefas]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tarefa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $dados = $request->all('tarefa', 'data_limite_conclusao');
+        $dados['user_id'] = auth()->user()->id;
+
+        $tarefa = Tarefa::create($dados);
+
+        $destinatario = auth()->user()->email;
+        Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
+        
+        return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarefa $tarefa)
+    {
+        $user_id = auth()->user()->id;
+        if($tarefa->user_id == $user_id) {
+            return view('tarefa.show', ['tarefa' => $tarefa]);
+        }
+        return view('acesso-negado');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        $user_id = auth()->user()->id;
+
+        if($tarefa->user_id == $user_id) {
+            return view('tarefa.edit', ['tarefa' => $tarefa]);
+        }
+        return view('acesso-negado');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        if(!$tarefa->user_id == auth()->user()->id) {
+            return view('acesso-negado');
+        }
+
+        $tarefa->update($request->all());
+        return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        if(!$tarefa->user_id == auth()->user()->id) {
+            return view('acesso-negado');
+        }
+        $tarefa->delete();
+        return redirect()->route('tarefa.index');
+    }
+
+    public function exportacao($extensao)
+    {
+        $nome_arquivo = 'lista_de_tarefas';
+        switch ($extensao) {
+            case 'xlsx': $nome_arquivo.='.xlsx'; break;
+            case 'csv': $nome_arquivo.='.csv'; break;            
+            case 'pdf': $nome_arquivo.='.pdf'; break;            
+            default: return redirect()->route('tarefa.index'); break;
+        }
+
+
+        return Excel::download(new TarefasExport, $nome_arquivo);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTesteMail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified'); */
+
+Route::get('tarefa/exportacao/{extensao}', 'App\http\Controllers\TarefaController@exportacao')->name('tarefa.exportacao');
+Route::resource('tarefa', 'App\http\Controllers\TarefaController')->middleware('verified');
+
+Route::get('/mensagem-teste', function() {
+    return new MensagemTesteMail();
+    /* Mail::to('laravelteste655@gmail.com')->send(new MensagemTesteMail());
+    return 'E-mail enviado com sucesso'; */
+});
